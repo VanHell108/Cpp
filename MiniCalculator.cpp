65880896ab3bfb70595fbb0e8b1b81cffe25a3fe
VanHell108 <80883697+VanHell108@users.noreply.github.com>
2022-11-28 00:15:29 +0200

    Create MiniCalculator.cpp

---
+++ MiniCalculator.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+int main()
+{
+
+     char op;
+     double num1;
+     double num2;
+     double result;
+
+     std::cout << "******** CALCULATOR ********\n";
+     std::cout << "Enter either (+ - * /): ";
+     std::cin >> op;
+     std::cout << "Enter first number: ";
+     std::cin >> num1;
+     std::cout << "Enter second number: ";
+     std::cin >> num2;
+
+     switch (op)
+     {
+     case '+':
+          result = num1 + num2;
+               std::cout << "Your result is: " << result;
+          break;
+     case '-':
+          result = num1 - num2;
+               std::cout << "Your result is: " << result;
+          break;
+     case '/':
+          result = num1 / num2;
+               std::cout << "Your result is: " << result;
+          break;
+     case '*':
+          result = num1 * num2;
+               std::cout << "Your result is: " << result;
+          break;
+     default:
+     std::cout << "You entered something else";
+          break;
+     }
+
+     std::cout << "\n******** CALCULATOR ********";
+
+     return 0;
+}
