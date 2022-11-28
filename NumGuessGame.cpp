1275419cedb10db065cd63e0720730f9e88a4f0c
VanHell108 <80883697+VanHell108@users.noreply.github.com>
2022-11-28 11:30:40 +0200

    Create NumGuessGame.cpp

---
+++ NumGuessGame.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+
+int main()
+{
+
+     int num;
+     int guess;
+     int tries;
+
+     srand(time(0));
+
+     num = (rand() % 20) + 1;
+
+     std::cout << "******** NUMBER GUESSING GAME ********\n";
+
+     do
+     {
+          std::cout << "Guess a number from 1 to 20: ";
+          std::cin >> guess;
+          tries++;
+
+          if (guess > num)
+          {
+               std::cout << "It's lower than that!\n";
+          }
+          else if (guess < num)
+          {
+               std::cout << "It's higher than that!";
+          }
+          else
+          {
+               std::cout << "You won!\nNumber of tries: "
+                         << tries;
+          }
+     } while (guess != num);
+
+     std::cout << "\n**************************************";
+
+     return 0;
+}
